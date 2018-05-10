6874d434e9ec77d0afb20f4deb69e72d0cfad645
Kenny Zhou <kezzhou@gmail.com>
2018-05-09 20:46:33 -0700

    Cleaned CSVExporter (#75)

--- CSVExporter.py
+++ CSVExporter.py
@@ -1,8 +1,4 @@
 #CSV Exporter, by Bryton 2/10/16
-#Last Updated: 2/19/18
-import utils
-from collections import OrderedDict
-from TBACommunicator import TBACommunicator
 import csv
 from DataModel import *
 import Math
@@ -35,7 +31,7 @@ def CSVExportTeam(comp, name, keys = []):
 #Puts some scouted and calculated data in csv file
 def CSVExportTIMD(comp, name, keys = []):
 	calculator = Math.Calculator(comp)
-	excluded = ['calculatedData', 'highShotTimesForBoilerAuto', 'highShotTimesForBoilerTele']
+	excluded = ['calculatedData']
 	with open('./EXPORT-' + name + '.csv', 'w') as f:
 		defaultKeys = [k for k in TeamInMatchData().__dict__.keys() if k not in excluded and k in keys]
 		defaultKeys += [k for k in TeamInMatchData().calculatedData.__dict__.keys() if k in keys]
@@ -63,50 +59,3 @@ def CSVExportMatch(comp, name, keys = []):
 			tDict.update(match.calculatedData.__dict__)
 			keys = sorted(defaultKeys, key = lambda k: (k != 'number', k.lower()))
 			writer.writerow({k : tDict[k] for k in keys})
-
-#Creates a dictionary of teams and their keys are the data associated with them
-def readOPRData(dataFilePath):
-	teamsDict = {}
-	wantedKeys = ['team Number', 'auto Fuel High','auto Scored Gears', 'teleop Scored Gears', 'teleop Takeoff Points']
-	with open(dataFilePath) as csvfile:
-		reader = csv.DictReader(csvfile)
-		first = True
-		keys = []
-		for r in reader:
-			teamsDict[r['team Number']] = {k : r[k] for k in wantedKeys}
-	return teamsDict
-
-#Gets data from tba and firebase to make csv file
-def CSVExportTeamOPRDataForComp(dataFilePath, dataOutputFilePath):
-	wantedKeys = ['team Number', 'auto Fuel High','auto Scored Gears', 'teleop Scored Gears', 'teleop Takeoff Points']
-	teams = TBACommunicator().makeEventTeamsRequest()
-	teamNums = [team['team_number'] for team in teams]
-	teamsDict = readOPRData(dataFilePath)
-	teamsDict = {k : v for k, v in teamsDict.items() if int(k) in teamNums}
-	# comp.updateTeamsAndMatchesFromFirebase()
-	with open(dataOutputFilePath, 'w') as f:
-		writer = csv.DictWriter(f, fieldnames = wantedKeys)
-		writer.writeheader()
-		for key, value in teamsDict.items():
-			writer.writerow({k : teamsDict[key][k] for k in wantedKeys})
-
-# CSVExportTeamOPRDataForComp('./ChampionshipHouston-Table 1.csv','./newton2017data.csv)
-
-#General export function for all of your basic data export needs
-def CSVExportTeamALL(comp):
-	CSVExportTeam(comp, 'TEAMALL', keys = Team().__dict__.keys() + Team().calculatedData.__dict__.keys())
-
-def CSVExportTIMDALL(comp):
-	CSVExportTIMD(comp, 'TIMDALL', keys = TeamInMatchData().__dict__.keys() + TeamInMatchData().calculatedData.__dict__.keys())
-
-def CSVExportMatchPredictedErrors(comp):
-	CSVExportMatch(comp, 'PREDICTEDERRORS', keys = ['number', 'redWinChance', 'blueWinChance'])
-
-def CSVExportMatchFoulComparison(comp):
-	CSVExportMatch(comp, 'FOULCOMPARISON', keys = ['number', 'foulPointsGainedBlue', 'foulPointsGainedRed', 'blueScore', 'redScore'])
-
-def CSVExportTeamRScores(comp):
-	CSVExportTeam(comp, 'RSCORES', keys = ['number', 'RScoreSpeed', 'RScoreAgility', 'RScoreDrivingAbility', 'RScoreDefense'])
-
-def CSVExportTeamRichard(comp, name):
-	CSVExportTeam(comp, name, keys = ['number', 'maxScaleCubes', 'avgCubesPlacedInScaleAuto', 'avgCubesPlacedInScaleTele', 'avgAllianceSwitchCubesAuto', 'avgAllianceSwitchCubesTele', 'avgOpponentSwitchCubesTele', 'avgNumCubesPlacedAuto', 'avgNumCubesPlacedTele', 'avgNumExchangeInputTele'])
