9f3fcd21c67ce9de747403935cdb116911093b8b
TheETKing <ethangtalbert@gmail.com>
2018-05-25 15:44:14 -0700

    Clean graph (#88)
    * Clean graph
    
    * Make requested changes to graph

--- graph.py
+++ graph.py
@@ -1,10 +1,13 @@
-#Last Updated: 8/26/17
 from matplotlib import pyplot as plt
 import matplotlib.mlab as m
 import csv
 import numpy as np
 
+#The purpose of this file is to create a histogram with a regression 
+#line to show and compare the precision rankings of scouts.
+
 dic = {}
+
 with open('./scoutRankExport.csv') as f:
 	reader = csv.DictReader(f)
 	for read in reader:
@@ -12,11 +15,15 @@ with open('./scoutRankExport.csv') as f:
 
 dic = {k : float(v) for k, v in dic.items()}
 keys = sorted(dic.keys(), key = lambda k: dic[k])
+
 for k in keys:
 	print(k, str(dic[k]))
+
 mu = np.mean(dic.values())
 sigma = np.std(dic.values())
+
 print(mu, '\n', sigma)
+
 plt.hist(dic.values(), normed = True)
 x = np.linspace(min(dic.values()), max(dic.values()))
 plt.plot(x, m.normpdf(x, mu, sigma))
