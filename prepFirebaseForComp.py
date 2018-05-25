73084a535de73fa9e1cd1a63b14422710f0379a8
TheETKing <ethangtalbert@gmail.com>
2018-05-25 15:45:05 -0700

    Clean prepFirebaseForComp (#91)
    * Clean prepFirebaseForComp
    
    * Make requested changes to prepFirebaseForComp

--- prepFirebaseForComp.py
+++ prepFirebaseForComp.py
@@ -1,23 +1,25 @@
-#Last Updated: 8/26/17
 import DataModel
 import firebaseCommunicator
 import TBACommunicator
 import utils
 
-############# Getting TBA Data ###################
-#Set to '<your initials>:TBA_communicator:0'
+#This file is meant to be run before each competition
+#to add teams and the match schedule to the firebase
 
 TBAC = TBACommunicator.TBACommunicator()
 PBC = firebaseCommunicator.PyrebaseCommunicator()
+
+#Prints out the firebase url so that the wrong firebase doesn't get wiped
 print(PBC.url)
+
 competition = DataModel.Competition(PBC)
 competition.eventCode = TBAC.code
 competition.PBC.JSONteams = TBAC.makeEventTeamsRequest()
 competition.PBC.JSONmatches = TBAC.makeEventMatchesRequest()
-#competition.PBC.wipeDatabase()
-#competition.PBC.addSlackProfilesToFirebase()
-#competition.PBC.addSingleKeysToFirebase()
-#competition.PBC.addTeamsToFirebase()
-competition.PBC.addSketchyMatchesToFirebase()
+competition.PBC.wipeDatabase()
+competition.PBC.addSlackProfilesToFirebase()
+competition.PBC.addSingleKeysToFirebase()
+competition.PBC.addTeamsToFirebase()
 competition.updateTeamsAndMatchesFromFirebase()
-competition.PBC.addTIMDsToFirebase(competition.matches) #You need to create the matches and teams before you call this
+#You need to create the matches and teams before you call this
+competition.PBC.addTIMDsToFirebase(competition.matches)
