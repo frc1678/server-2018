6c849e1bf164f8600e5f6e59c0b1dffbf91af5b7
TheETKing <ethangtalbert@gmail.com>
2018-05-25 15:44:45 -0700

    Clean preScouting (#90)
    * Clean preScouting
    
    * Make requested changes to preScouting

--- preScouting.py
+++ preScouting.py
@@ -4,6 +4,10 @@ import DataModel
 import utils
 import csv
 
+#The purpose of this file is to create a csv file filled with data about 
+#all of the teams in a competition from all of their previous competitions, 
+#including autoRun, climb, autoScale, and autoSwitch datapoints
+
 TBAC = TBACommunicator.TBACommunicator()
 pbc = firebaseCommunicator.PyrebaseCommunicator()
 comp = DataModel.Competition(pbc)
@@ -16,7 +20,7 @@ masterList = []
 print('Begining prescouting...')
 auto = {}
 for team in teams:
-	full = {event['event_code'] : event['week'] for event in TBAC.makeTeamEventsRequest(team) if event['event_code'] != 'new'}
+	full = {event['event_code'] : event['week'] for event in TBAC.makeTeamEventsRequest(team)}
 	events = sorted([event for event in full.keys()], key = lambda event: full[event])
 	event = events[-1]
 	autoRuns = []
@@ -25,14 +29,14 @@ for team in teams:
 	autoSwitches = []
 	print('Prescouting for ' + team.split('c')[1] + '...')
 	for event in events:
-	matches = TBAC.makeRequest(TBAC.basicURL + 'team/' + team + '/event/' + str(TBAC.year) + event + '/matches')
-	for match in matches:
-		allianceColor = 'blue' if team in match['alliances']['blue']['team_keys'] else 'red'
-		robotNum = (match['alliances'][allianceColor]['team_keys'].index(team)) + 1
-		autoRuns += [True if match['score_breakdown'][allianceColor]['autoRobot' + str(robotNum)]  == 'AutoRun' else False]
-		climbs += [True if match['score_breakdown'][allianceColor]['endgameRobot' + str(robotNum)]  == 'Climbing' else False]
-		autoScales += [match['score_breakdown'][allianceColor]['autoScaleOwnershipSec']]
-		autoSwitches += [match['score_breakdown'][allianceColor]['autoSwitchOwnershipSec']]
+		matches = TBAC.makeRequest(TBAC.basicURL + 'team/' + team + '/event/' + str(TBAC.year) + event + '/matches')
+		for match in matches:
+			allianceColor = 'blue' if team in match['alliances']['blue']['team_keys'] else 'red'
+			robotNum = (match['alliances'][allianceColor]['team_keys'].index(team)) + 1
+			autoRuns += [True if match['score_breakdown'][allianceColor]['autoRobot' + str(robotNum)]  == 'AutoRun' else False]
+			climbs += [True if match['score_breakdown'][allianceColor]['endgameRobot' + str(robotNum)]  == 'Climbing' else False]
+			autoScales += [match['score_breakdown'][allianceColor]['autoScaleOwnershipSec']]
+			autoSwitches += [match['score_breakdown'][allianceColor]['autoSwitchOwnershipSec']]
 	if events:
 		masterList += [{'team' : team, 'autoRun' : utils.avg(autoRuns), 'climb' : utils.avg(climbs), 'autoScale' : utils.avg(autoScales), 'autoSwitch' : utils.avg(autoSwitches)}]
 	else:
